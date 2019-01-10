a2a6db9d2373ac695c5d7436832dc5eca0965ca1
niva25 <niva.israelov@gmail.com>
2019-01-10 19:59:26 +0200

    fix bug in login btn

--- private/shared/login_button.php
+++ private/shared/login_button.php
@@ -1,5 +1,5 @@
 <?php
-if (is_logged_in()) {
+if (!is_logged_in()) {
     ?>
 <button class="btn btn btn-primary btn-lg col-sm">log in</button>
 <button class="btn btn-secondary btn-lg col-sm">registration</button>
