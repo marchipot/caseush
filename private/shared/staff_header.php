02a368cb9a1d936cb95c14481f45c502bd5dba6f
Dani Marchi <danielmarchi.tech@gamil.com>
2019-01-11 10:07:22 +0200

    fix ui bug

--- private/shared/staff_header.php
+++ private/shared/staff_header.php
@@ -43,16 +43,6 @@
   <span class="icon icon-bars overlay"></span>
 </div>
 
-
-<div class="overlay-menu">
-  <ul id="menu">
-      <li><a href="#home">Home</a></li>
-      <li><a href="#services">Services</a></li>
-      <li><a href="#about">About</a></li>
-      <li><a href="#contact">Contact</a></li>
-    </ul>
-</div>
-
 </navigation>
 
 
