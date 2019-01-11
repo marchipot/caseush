4dbba351e66eaf0d9ca6e4b848e508f0a871d22f
niva25 <niva.israelov@gmail.com>
2019-01-11 10:41:44 +0200

    add styling

--- public/login.php
+++ public/login.php
@@ -4,12 +4,20 @@ $page_title="login";
 ?>
 
 <?php include(SHARED_PATH . '/staff_header.php'); ?>
-
-    <form action="login_process.php" method="post">
-        <input type="text" name="username" placeholder="username" >
-        <input type="password" name="password" placeholder="password" >
-        <input type="submit" name="submit"  >
-    </form>
+<div class="jumbotron">
+    <h4 class="logIn">התחברות</h4>
+        <form  class="logIn" action="login_process.php" method="post">
+            <div class="form-group">
+                <label for="exampleInputUsername1">שם משתמש</label>
+                <input type="text" name="username" class="form-control" id="exampleInputUsername1" placeholder="username">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">סיסמא</label>
+                <input type="password" name="password" class="form-control" id="exampleInputPassword1" placeholder="password">
+            </div>
+            <button type="submit" name="submit" class="btn btn-primary btn-lg">שלח</button>
+        </form>
+</div>
 </body>
 </html>
 
