d4fc5e1b24e356ac7c1bc77236fb4b543e977a38
niva25 <niva.israelov@gmail.com>
2019-01-14 15:08:11 +0200

    changes

--- private/initialize.php
+++ private/initialize.php
@@ -21,7 +21,7 @@ $doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
 define("WWW_ROOT", $doc_root);
 
 
-npm install nodemailer --saverequire_once('functions.php');
+require_once('functions.php');
 require_once('database.php');
 require_once('query_functions.php');
 require_once('validation_functions.php');
